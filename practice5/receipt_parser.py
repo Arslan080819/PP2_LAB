9b6efb3f4f9970063b14f9c228424da1e8885619
arslan07 <zhumalin08@mail.ru>
2026-03-20 12:22:30 +0500

    123

--- practice5/receipt_parser.py
+++ practice5/receipt_parser.py
@@ -3,22 +3,22 @@ import json
 with open(r"\Users\Адият\OneDrive - АО Казахстанско-Британский Технический Университет\Рабочий стол\PP2_LAB\practice5\raw.txt", encoding="utf-8") as f:
     text = f.read()
 
-# Prices
+#Prices
 prices = [float(p.replace(" ", "").replace(",", ".")) 
           for p in re.findall(r"\n([\d ]+,\d{2})\nСтоимость", text)]
 
-# Product names
+#Product names
 products = re.findall(r"\d+\.\n(.+)", text)
 
-# Total
+#Total
 total_match = re.search(r"ИТОГО:\n([\d ]+,\d{2})", text)
 total = total_match.group(1) if total_match else None
 
-# Date and Time
+#Date and Time
 datetime_match = re.search(r"Время:\s*(\d{2}\.\d{2}\.\d{4}\s+\d{2}:\d{2}:\d{2})", text)
 datetime = datetime_match.group(1) if datetime_match else None
 
-# Payment Method
+#Payment Method
 payment_match = re.search(r"(Банковская карта)", text)
 payment_method = payment_match.group(1) if payment_match else None
 
